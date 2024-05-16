6a070a2d6cd266e28ca8a8b83d67df264bfdebc2
rfsyhb <rafisyihab3@gmail.com>
2024-05-16 23:03:51 +0700

    add Card w/ styled comp

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -1,22 +1,24 @@
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import Card from './styled/Card';
+import Container from './styled/Container';
 
 export default function RestaurantItem({ id, pictureId, name, description }) {
   return (
-    <div className="card">
+    <Card>
       <img
         src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`}
         alt={name}
       />
 
-      <div className="container">
+      <Container>
         <h2>
           <Link href={`detail/${id}`}>{name}</Link>
         </h2>
         <p>{description}</p>
         <br />
-      </div>
-    </div>
+      </Container>
+    </Card>
   );
 }
 
